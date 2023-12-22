ac8ffe16c47748e61b9af44567999291c57d3326
Francisco Borquez <fborquez@outlook.com>
2023-12-21 22:15:40 -0300

    add orderBy option

--- app/Http/Controllers/SubpanelController.php
+++ app/Http/Controllers/SubpanelController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Subpanel;
+use Illuminate\Http\Request;
 
 class SubpanelController extends Controller
 {
-    public function index() {
-        return Subpanel::with('configuraciones')->with('configuraciones.opciones')->get();
+    public function index(Request $request) {
+        $subpanels = new Subpanel();
+        $subpanels = $subpanels->with('configuraciones')->with('configuraciones.opciones');
+
+        if ($request->has('orderBy')) {
+            $subpanels->orderBy($request->get('orderBy'), $request->get('orderDirection', 'DESC'));
+        }
+
+        return $subpanels->get();
     }
 }
